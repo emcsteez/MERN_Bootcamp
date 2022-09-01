e8ada952686406e609adcd9e147a58a41f59ed02
Elijah Bowers <elijahb603dev@gmail.com>
2022-09-01 14:14:24 -0400

    First JSX assignment

---
+++ React/first_jsx/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+
+        <h3>Things I need to do:</h3>
+
+        <ul>
+          <li>Pick up dog food</li>
+          <li>Clean up around house</li>
+          <li>Play a round of disc golf</li>
+          <li> <u>PRACTICE CODING!!</u> </li>
+        </ul>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
