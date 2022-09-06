c5132585af24c4bd0fccefb71fb73399f287e9da
Elijah Bowers <elijahb603dev@gmail.com>
2022-09-06 13:43:39 -0400

    Putting it Together assignment

---
+++ putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor} = props;
+    const [stateAge, setStateAge] = useState(age);
+    return(
+        <div>
+            <h1>{lastName} {firstName}</h1>
+            <p>Age: {stateAge}</p>
+            <p>Hair color: {hairColor}</p>
+            <button onClick={() => setStateAge(stateAge + 1)}>
+                Birthday Button for {firstName} {lastName}
+            </button>
+        </div>
+    );
+}
+export default PersonCard;
